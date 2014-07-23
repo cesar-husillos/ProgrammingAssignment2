ce3b75f0d6b520c0776125058fad31660f0d2c1b
Cesar Husillos <cesar_husillos@yahoo.es>
2014-07-23 19:52:16 +0200

    Comments added to source code.

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv <- NULL
+        ## Function that caches inverse matrix
+        
+        ## Cache variable for the inverse matrix
+        inv <- NULL  
+        
+        ## Function that initializes the original matrix and inverse one (last to NULL)
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
+        
+        ## Function that returns original matrix ( if not assigned, returns NA (1x1 matrix))
         get <- function() x
+        
+        ## Function that assigns inverse matrix to the one given as parameter
         setinv <- function(matinv) inv <<- matinv
+        
+        ## Function that returns inverse matrix (NULL if not assigned)
         getinv <- function() inv
+        
+        ## List of funtions associated to data/object returned by this function
         list(set = set, get = get,
                 setinv = setinv,
                 getinv = getinv)
@@ -22,13 +35,22 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        ## Initialize variable 'inv' to the value stored on input parameter ('x')
         inv <- x$getinv()
+        
+        ## If there is a value for inverse matrix in input parameter 'x', it returns cached value
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
         }
+        
+        ## If this value is NULL (not inverse computed/assigned), we computed inverse for 
+        ## original given matrix cached in parameter 'x'
         mat <- x$get()
         inv <- solve(mat, ...)
+        
+        ## Assign computed inverse matrix to input parameter 'x' and return inverse
         x$setinv(inv)
         inv
 }
